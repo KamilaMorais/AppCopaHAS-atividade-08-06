7471c2d34d55ba8c8e8f600f8b527435663a1c35
KamilaMorais <kamilamorais.ctt@gmail.com>
2026-06-08 20:12:13 -0300

    aula 16 - 08/06

---
+++ Services/JogoService.cs
@@ -0,0 +1,48 @@
+﻿using AppCopaHAS.Models;
+using AppCopaHAS.Models.DTOs;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace AppCopaHAS.Services
+{
+    public class JogoService : Request
+    {
+        private readonly Request _request;
+        private const string _apiUrlBase = "https://copaapi3ai.azurewebsites.net/Jogos";
+
+        public JogoService()
+        {
+            _request = new Request();
+        }
+
+        public async Task<ObservableCollection<Jogo>> GetJogosAsync()
+        {
+            string urlComplementar = string.Format("{0}", "/GetAll");
+
+            ObservableCollection<Jogo> lista =
+                await _request.GetAsync<ObservableCollection<Jogo>>(
+                    _apiUrlBase + urlComplementar,
+                    string.Empty);
+
+            return lista;
+        }
+
+        //método para obter os jogos cadastrados.
+        public async Task<ObservableCollection<JogoDTO>> GetJogosDTOAsync()
+        {
+            string urlComplementar = string.Format("{0}", "/ObterTabela");
+            ObservableCollection<JogoDTO> lista = 
+                await _request.GetAsync<ObservableCollection<JogoDTO>>(_apiUrlBase + urlComplementar, string.Empty);
+            return lista;
+        }
+
+        //método que receberá um objeto do tipo Jogo e usará a classe Request para enviar o jogo para a API.
+        public async Task<Jogo> PostJogoAsync(Jogo j)
+        {
+            Jogo jogoInserido = await _request.PostAsync<Jogo>(_apiUrlBase, j, string.Empty);
+            return jogoInserido;
+        }
+
+
+    }
+}
---
+++ ViewModels/TabelaViewModel.cs
@@ -0,0 +1,35 @@
+﻿
+
+using AppCopaHAS.Models.DTOs;
+using AppCopaHAS.Services;
+using System.Collections.ObjectModel;
+
+namespace AppCopaHAS.ViewModels
+{
+    //Herança da classe BaseViewModel.
+    public class TabelaViewModel : BaseViewModel
+    {
+        JogoService _jogoService;
+        public ObservableCollection<JogoDTO> Jogos { get; set; }
+
+        public TabelaViewModel()
+        {
+            _jogoService = new JogoService();
+            Jogos = new ObservableCollection<JogoDTO>();
+            _ = ObterJogos();
+        }
+
+        public async Task ObterJogos()
+        {
+            try
+            {
+                Jogos = await _jogoService.GetJogosDTOAsync();
+                OnPropertyChanged(nameof(Jogos));
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Ops", ex.Message + " Detalhes: " + ex.InnerException?.Message, "Ok");
+            }
+        }
+    }
+}
